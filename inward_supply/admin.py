cbf8822e208d0f7048734830f449e32bf3268b2d
Ch.V.Sai Koushik <saikoushik.chundi@gmail.com>
2025-02-09 23:57:41 +0530

    Added Product,Supplier,Invoice bill model and migrations for inventory and inward_supply

--- inward_supply/admin.py
+++ inward_supply/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Supplier, InwardInvoiceBill
 
-# Register your models here.
+admin.site.register(Supplier)
+admin.site.register(InwardInvoiceBill)
--- inward_supply/models.py
+++ inward_supply/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from inventory.models import Product
+class Supplier(models.Model):
+    firm_name = models.CharField(max_length=255)
+    person_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=10, unique=True)
+    email_id = models.EmailField(unique=True)
+    address = models.CharField(max_length=255)
+    debit = models.FloatField(default=0.0)
+    total_sales = models.IntegerField(default=0)
+    gst = models.CharField(max_length=20, unique=True)
 
-# Create your models here.
+    def __str__(self):
+        return f"{self.firm_name} ({self.person_name})"
+
+class InwardInvoiceBill(models.Model):
+    bill_number = models.CharField(max_length=50, unique=True)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    date = models.DateField()
+    product_list = models.ManyToManyField(Product)
+    final_amount = models.FloatField()
+
+    def __str__(self):
+        return f"Bill {self.bill_number} - {self.supplier.name}"
+
