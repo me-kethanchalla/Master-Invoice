5fa0a33cafe4af60de66879561450f9a6fda7285
Swift <hellosai109@gmail.com>
2025-03-17 19:39:33 +0530

    Completed the Transactions part, incl. the 'subtraction of credit/debit after adding a new payment' part, added the navbar specs and made necessary changes to any/all models

--- transactions/models.py
+++ transactions/models.py
@@ -2,5 +2,17 @@ from django.db import models
 from inventory.models import Inventory
 from inward_supply.models import Supplier
 from outward_supply.models import Retailer
+from datetime import date
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    firm_name = models.CharField(max_length=255)
+    person_name = models.CharField(max_length=255)
+    payment = models.FloatField(default=0.0)
+    remarks = models.CharField(max_length=1023)
+    type = models.IntegerField(default=0)
+    def __str__(self):
+        return f"{self.firm_name}"
--- transactions/views.py
+++ transactions/views.py
@@ -2,14 +2,116 @@ from django.shortcuts import render, get_object_or_404, redirect
 from inventory.models import Inventory
 from inward_supply.models import Supplier
 from outward_supply.models import Retailer
+from .models import Transaction
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 def pending(request):
-    suppliers = Supplier.objects.all()
-    retailers = Retailer.objects.all()
-    return render(request, 'transactions/pending.html', {'suppliers': suppliers, 'retailers': retailers})
+ suppliers = Supplier.objects.all()
+ retailers = Retailer.objects.all()
+ return render(request, 'transactions/pending.html', {'suppliers': suppliers, 'retailers': retailers})
+
+def view_transaction_history(request):
+ suppliers = Supplier.objects.all()
+ retailers = Retailer.objects.all()
+ transactions = Transaction.objects.all()
+ return render(request, 'transactions/view_transaction_history.html', {'suppliers': suppliers, 'retailers': retailers, 'transactions': transactions})
+
 def clear_retailer_debit(request):
-    retailers = Retailer.objects.all()
-    return render(request, 'transactions/clear_retailer_debit.html',{'retailers': retailers})
+ retailers = Retailer.objects.all()
+ return render(request, 'transactions/clear_retailer_debit.html',{'retailers': retailers})
 def clear_supplier_debit(request):
+ suppliers = Supplier.objects.all()
+ return render(request, 'transactions/clear_supplier_debit.html', {'suppliers': suppliers})
+
+@login_required
+def update_transaction_supplier(request):
     suppliers = Supplier.objects.all()
-    return render(request, 'transactions/clear_supplier_debit.html', {'suppliers': suppliers})
+    retailers = Retailer.objects.all()
+    if request.method == 'POST':
+        transactions = request.POST.getlist('firm_name[]')
+        amounts = request.POST.getlist('amount_paid[]')
+        remarks = request.POST.getlist('remarks[]')
+        # date = request.POST.get('date')
+        for i in range(len(transactions)):
+            firm_name = transactions[i]
+            payment = float(amounts[i])
+            remark = remarks[i]
+            person_name = None
+            for supplier in suppliers:
+                if supplier.firm_name == firm_name:
+                    person_name = supplier.person_name
+                    supplier.debit = supplier.debit - payment
+                    supplier.save()
+                    break
+            if not person_name:
+                person_name = "Unknown"
+                Transaction.objects.create(
+                user=request.user,
+                firm_name=firm_name,
+                person_name=person_name,
+                # date=date,
+                payment=payment,
+                remarks=remark,
+                type=0
+            )
+            else:
+               
+               Transaction.objects.create(
+                user=request.user,
+                firm_name=firm_name,
+                person_name=person_name,
+                # date=date,
+                payment=payment,
+                remarks=remark,
+                type=0
+            )
+        messages.success(request, "Transactions updated successfully!")
+        return redirect('pending')
+    return render(request, 'transactions/clear_supplier_debit.html', {'suppliers': suppliers, 'retailers': retailers})
+
+
+@login_required
+def update_transaction_retailer(request):
+    suppliers = Supplier.objects.all()
+    retailers = Retailer.objects.all()
+    if request.method == 'POST':
+        transactions = request.POST.getlist('firm_name[]')
+        amounts = request.POST.getlist('amount_paid[]')
+        remarks = request.POST.getlist('remarks[]')
+        # date = request.POST.get('date')
+        for i in range(len(transactions)):
+            firm_name = transactions[i]
+            payment = float(amounts[i])
+            remark = remarks[i]
+            person_name = None
+            for retailer in retailers:
+                if retailer.firm_name == firm_name:
+                    person_name = retailer.person_name
+                    retailer.credit = retailer.credit - payment
+                    retailer.save()
+                    break
+            if not person_name:
+                person_name = "Unknown"
+                Transaction.objects.create(
+                user=request.user,
+                firm_name=firm_name,
+                person_name=person_name,
+                # date=date,
+                payment=payment,
+                remarks=remark,
+                type=1
+            )
+            else:
+                Transaction.objects.create(
+                user=request.user,
+                firm_name=firm_name,
+                person_name=person_name,
+                # date=date,
+                payment=payment,
+                remarks=remark,
+                type=1
+            )
+        messages.success(request, "Transactions updated successfully!")
+        return redirect('pending')
+    return render(request, 'transactions/pending.html', {'suppliers': suppliers, 'retailers': retailers})
