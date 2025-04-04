ca66a6335226e65156f53444c97095db2e8fdb95
kundansai-138 <ckundan999@gmail.com>
2025-04-04 16:35:30 +0530

    fix postsql

--- MasterInvoice/settings.py
+++ MasterInvoice/settings.py
@@ -88,11 +88,11 @@ LOGIN_URL = '/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
+
 
 
 # Password validation
