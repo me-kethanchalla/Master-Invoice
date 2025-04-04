8159a95eb56de79fc93e961bce89d50b3b8ad8ed
kundansai-138 <ckundan999@gmail.com>
2025-04-04 12:32:46 +0530

    fic

--- MasterInvoice/settings.py
+++ MasterInvoice/settings.py
@@ -88,7 +88,10 @@ LOGIN_URL = '/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
